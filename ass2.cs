5aa29fc6a46964c7c3e7e0351fbd98b326b21f9e
blessonsaju <58213411+blessonsaju@users.noreply.github.com>
2022-11-27 11:47:01 +0530

    Add files via upload

---
+++ ass2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duearray
+{
+     class ass2
+    {
+        public  virtual void Display()
+        {
+            Console.WriteLine("blesson saju");
+        }
+    }
+
+    public class Dclass : ass2 //derived class
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Welcome to Nest Digital");
+        }
+    }
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            ass2 d = new ass2();
+            d.Display();
+            Dclass b = new Dclass();
+            b.Display();
+
+        }
+    }
+}
