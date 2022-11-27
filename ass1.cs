ad43d94e3454e8c4ac0edafea956296900c82b5d
blessonsaju <58213411+blessonsaju@users.noreply.github.com>
2022-11-27 11:30:39 +0530

    Add files via upload

---
+++ ass1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duearray
+{
+     class ass1
+    {
+
+         public string name;
+         public int rollnum;
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Roll Number: " + rollnum);
+        }
+    }
+
+    class abc
+    {
+        public static void Main(string[] args)
+        {
+            ass1 s1 = new ass1();
+            s1.name = "saju";
+            s1.rollnum = 1;
+
+            ass1 s2 = new ass1();
+            s2.name = "blesson";
+            s2.rollnum = 2;
+
+            s1.DisplayDetails();
+            s2.DisplayDetails();
+        }
+    }
+}
+
